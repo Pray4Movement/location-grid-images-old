d1dc4b09e2cd9f8677039d70f45b9982dfc804d4
Chris Chasm <chris@chasm.solutions>
2022-03-28 08:59:23 -0600

    initialize folder

---
+++ README.md
@@ -0,0 +1,18 @@
+# Prayer Global Porch Image
+
+Prayer Global Porch Images is a support repository for the images used by the Prayer Global Porch app.
+
+## Purpose
+
+Because of the collaborative nature of the supporting material for the prayer porch, we are
+hosting the assets for the prayer global site in this repo to support the collaboration around the app.
+
+## Usage
+
+### Do:
+ - Optimize and compress and size all files before upload to the repo.
+
+### Do Not:
+ - Add full resolution files or photo creation assets
+ - Add large files
+
---
+++ prayer-global-porch-images.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name: Prayer.Global Porch Images
+ * Plugin URI: https://github.com/Pray4Movement/prayer-global-porch-images
+ * Description: Support image library for Prayer.Global front porch app for Disciple.Tools
+ * Text Domain: prayer-global-porch-images
+ * Domain Path: /languages
+ * Version:  0.1
+ * Author URI: https://github.com/Pray4movement
+ * GitHub Plugin URI: https://github.com/Pray4Movement/prayer-global-porch-images
+ * Requires at least: 4.7.0
+ * (Requires 4.7+ because of the integration of the REST API at 4.7 and the security requirements of this milestone version.)
+ * Tested up to: 5.9
+ *
+ * @package Disciple_Tools
+ * @link    https://github.com/DiscipleTools
+ * @license GPL-2.0 or later
+ *          https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
